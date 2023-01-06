1f501d091e40791208d9bfdba6019320cc2cad8f
Al Turing <jongs@Jons-iMac.local>
2023-01-06 19:00:05 +0000

    Improvement to indentation.

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,50 @@
 // Declare variables
-let gameOptions = ['rock', 'paper', 'scissors']
-let cpuChoice = '';
-let userChoice = '';
+let gameOptions = ["rock", "paper", "scissors"];
+let cpuChoice = "";
+let userChoice = "";
 let cpuPoints = 0;
 let userPoints = 0;
-let output = '';
+let output = "";
 
 // While loop until either CPU or User reaches 2 points (best of 3)
 do {
   // CPU makes a choice
   cpuChoice = gameOptions[Math.floor(Math.random() * gameOptions.length)];
   // User makes a choice
-  userChoice = prompt('What is your choice? Enter rock, paper or scissors.').toLowerCase();
+  userChoice = prompt(
+    "What is your choice? Enter rock, paper or scissors."
+  ).toLowerCase();
   // While loop if user enters choice incorrectly
   while (!gameOptions.includes(userChoice)) {
-  userChoice = prompt('Make sure you enter your choice correctly: Enter rock, paper or scissors.').toLowerCase();
+    userChoice = prompt(
+      "Make sure you enter your choice correctly: Enter rock, paper or scissors."
+    ).toLowerCase();
   }
 
   // No points awarded as it was a tie
   if (userChoice === cpuChoice) {
     alert(`The computer chose ${cpuChoice}. That was a draw!`);
-  }
-  // Check whether user wins the round
-  else if ((userChoice === 'rock' && cpuChoice === 'scissors') ||
-  (userChoice === 'scissors' && cpuChoice === 'paper') ||
-  (userChoice === 'paper' && cpuChoice === 'rock')) {
+  } else if ( // Check whether user wins the round
+    (userChoice === "rock" && cpuChoice === "scissors") ||
+    (userChoice === "scissors" && cpuChoice === "paper") ||
+    (userChoice === "paper" && cpuChoice === "rock")
+  ) {
     // Add 1 to the user's current score
-    userPoints ++;
-    alert(`Congratulations, the computer chose ${cpuChoice}. You won that round!`);
-}
-  else {
+    userPoints++;
+    alert(
+      `Congratulations, the computer chose ${cpuChoice}. You won that round!`
+    );
+  } else {
     // Add 1 to cpu's current score
-    cpuPoints ++;
+    cpuPoints++;
     alert(`Ouch, the computer chose ${cpuChoice}. You lost that round!`);
   }
-} while (userPoints < 2 && cpuPoints < 2)
+} while (userPoints < 2 && cpuPoints < 2);
 
 // Create post-game output
-if(cpuPoints > userPoints) {
+if (cpuPoints > userPoints) {
   output = `Unlucky. You lose! The computer scored ${cpuPoints}, you scored ${userPoints}.`;
-}
-else {
+} else {
   output = `Congratulations. You win! The computer scored ${cpuPoints}, you scored ${userPoints}.`;
 }
 
